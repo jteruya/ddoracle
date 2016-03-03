e1e2317075d12d13d1d274b0108ffbcd7e4b8b3f
Jonathan Teruya <jonathanteruya@DDs-MacBook-Pro-5.local>
2016-03-02 16:32:54 -0800

    new file:   activeusers_eventlevel.sql 	new file:   adoption_eventlevel.sql 	new file:   appsessions_eventlevel.sql

---
+++ weeklyemail/sql/activeusers_eventlevel.sql
@@ -0,0 +1,18 @@
+\pset footer
+\f ','
+\a
+\o :csvdir/activeusers_eventlevel.csv
+
+select week_starting as "Week Of"
+     , applicationid as "Application ID"
+     , name as "Event Name"
+     , startdate as "Event Start Date"
+     , enddate as "Event End Date"
+     , eventtype as "Event Type"
+     , case
+          when openevent = 1 then 'Open Reg Event'
+          else 'Closed Reg Event'
+       end as "Event Reg Type"
+     , usersactive as "Active Users"
+from dashboard.weekly_adoption_events
+order by 1,3,4,5,2;
---
+++ weeklyemail/sql/adoption_eventlevel.sql
@@ -0,0 +1,16 @@
+\pset footer
+\f ','
+\a
+\o :csvdir/adoption_eventlevel.csv
+
+select week_starting as "Week Of"
+     , applicationid as "Application ID"
+     , name as "Event Name"
+     , startdate as "Event Start Date"
+     , enddate as "Event End Date"
+     , registrants as "Users"
+     , usersactive as "Active Users"
+     , adoption as "Adoption %" 
+from dashboard.weekly_adoption_events
+where openevent = 0
+order by 1,3,4,2;
