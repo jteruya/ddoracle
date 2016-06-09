f2a0f0220aefe5925aed712122b8ed8dd7f8757e
Jonathan Teruya <jonathanteruya@DDs-MacBook-Pro-5.local>
2016-06-09 16:42:37 -0700

    modified:   ../../phd12_login.sh

--- phd12_login.sh
+++ phd12_login.sh
@@ -21,8 +21,8 @@
 kpi_domain="login"
 wd="oracle"
 etl="${kpi_domain}_etl"
-#domain_wd="$HOME/${wd}/${kpi_domain}"
-domain_wd="$HOME/repo/${wd}/${kpi_domain}"
+domain_wd="$HOME/${wd}/${kpi_domain}"
+#domain_wd="$HOME/repo/${wd}/${kpi_domain}"
 
 # Report fields
 kpi_ios_open_login_funnel_devices_monthly_report="KPI_Robin_iOSOpenLoginFunnelDevicesMonthly"
@@ -37,8 +37,8 @@ kpi_android_closed_login_funnel_monthly_report="KPI_Robin_AndroidClosedLoginFunn
 
 # Generic Tools/Scripts
 run_sql_robin='psql -h 10.223.192.6 -p 5432 -U etl -A -F"," analytics -f '
-#transpose="python $HOME/tools/transpose.py"
-transpose="python $HOME/repo/tools/transpose.py"
+transpose="python $HOME/tools/transpose.py"
+#transpose="python $HOME/repo/tools/transpose.py"
 index_insights_fill="python ${kpi_domain_wd}/index_insights_fill.py"
 email_reports_wd="$HOME/email_reports"
 
