dcd705ba449c852f565a856b95403a63a09c425a
Jonathan Teruya <jonathanteruya@DDs-MacBook-Pro-5.local>
2016-06-21 00:25:11 -0700

    modified:   KPI_Robin_SessionDurations.sql

--- main/sql/KPI_Robin_SessionDurations.sql
+++ main/sql/KPI_Robin_SessionDurations.sql
@@ -1,4 +1,5 @@
 --Identify the Median Length of a Session per Week (for the past 8 weeks)
 SELECT YYYY_WW, ROUND(CAST(PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY Duration_Seconds) AS NUMERIC),2) AS Median_SecondsPerSession 
 FROM dashboard.Session_Durations_Weekly 
-GROUP BY 1;
+GROUP BY 1
+ORDER BY ;
