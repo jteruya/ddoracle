bd3bc2dee7258c58937bb09ea9a124e98b22c432
Jonathan Teruya <jonathan@doubledutch.me>
2016-07-27 16:26:04 -0700

    modified:   KPI_Robin_SessionOSDistribution.sql 	modified:   KPI_Robin_UserOSDistribution.sql

--- misc/sql/KPI_Robin_UserOSDistribution.sql
+++ misc/sql/KPI_Robin_UserOSDistribution.sql
@@ -3,7 +3,7 @@ SELECT CASE WHEN AppTypeId = 1 THEN 'iPhone' WHEN AppTypeId = 2 THEN 'iPad' WHEN
 --CNT, SUM(CNT) OVER (PARTITION BY YYYY_MM) AS Total 
 ROUND(100 * CAST(CNT AS NUMERIC) / CAST(SUM(CNT) OVER (PARTITION BY YYYY_MM) AS NUMERIC),2) AS PCT_CNT
 FROM (
-        SELECT YYYY_MM, AppTypeId, COUNT(DISTINCT UserId) AS CNT 
+        SELECT YYYY_MM, CASE WHEN AppTypeId = 2 THEN 1 ELSE AppTypeId END AS AppTypeId, COUNT(DISTINCT UserId) AS CNT 
         FROM dashboard.kpi_social_metrics_firstsession_os_version
         GROUP BY 1,2
 ) t
